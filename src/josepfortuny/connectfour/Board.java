ba014c0707c2bad5615a616b685885e736cb91b7
Josep Fortuny <82282886+josep42ny@users.noreply.github.com>
2025-02-05 12:51:55 +0100

    add size constructor for board class

--- src/josepfortuny/connectfour/Board.java
+++ src/josepfortuny/connectfour/Board.java
@@ -2,14 +2,22 @@ package josepfortuny.connectfour;
 
 public class Board {
 
+    private int rows;
+    private int columns;
     private Character[][] playGrid = new Character[6][7];
+
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+    }
 
     public Character[][] getPlayGrid() {
         return playGrid;
     }
 
     public void play(Character character, int column) {
-        playGrid[0][column] = character;
+
+
     }
 
     private int findFreeRow(int column) {
@@ -21,7 +29,7 @@ public class Board {
         return -1;
     }
 
-    private boolean canDropPiece(int column) {
+    public boolean canDropPiece(int column) {
         return playGrid[0][column] == null;
     }
 

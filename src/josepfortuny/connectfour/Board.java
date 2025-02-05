8c95e022980b632837535806bd8984d0cebe14af
Josep Fortuny <82282886+josep42ny@users.noreply.github.com>
2025-02-05 12:32:58 +0100

    add method to board to find free row

--- src/josepfortuny/connectfour/Board.java
+++ src/josepfortuny/connectfour/Board.java
@@ -12,4 +12,17 @@ public class Board {
         playGrid[0][column] = character;
     }
 
+    private int findFreeRow(int column) {
+        for (int row = playGrid.length - 1; row >= 0; row--) {
+            if (playGrid[row][column] == null) {
+                return row;
+            }
+        }
+        return -1;
+    }
+
+    private boolean canDropPiece(int column) {
+        return playGrid[0][column] == null;
+    }
+
 }

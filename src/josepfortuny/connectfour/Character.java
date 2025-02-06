a49cdf5b7e00f6c602fb7869498acbaad3376443
Josep Fortuny <82282886+josep42ny@users.noreply.github.com>
2025-02-06 10:03:09 +0100

    move colors to characters

--- src/josepfortuny/connectfour/Character.java
+++ src/josepfortuny/connectfour/Character.java
@@ -2,11 +2,15 @@ package josepfortuny.connectfour;
 
 public class Character {
 
+    private static final String[] COLORS = {AnsiCodes.ANSI_CYAN, AnsiCodes.ANSI_YELLOW};
+    private static int count = 0;
+    private int id;
     private String name;
     private String ansiColor;
 
-    public Character(String name, String ansiColor) {
-        this.ansiColor = ansiColor;
+    public Character(String name) {
+        this.ansiColor = COLORS[count];
+        this.id = count++;
     }
 
     public String getName() {
@@ -16,4 +20,9 @@ public class Character {
     public String getAnsiColor() {
         return ansiColor;
     }
+
+    public static String getAnsiColorFromCharacter(int id) {
+        return COLORS[id];
+    }
+
 }
--- src/josepfortuny/connectfour/InputHandler.java
+++ src/josepfortuny/connectfour/InputHandler.java
@@ -2,19 +2,23 @@ package josepfortuny.connectfour;
 
 public class InputHandler {
 
-    private static final String[] COLORS = {AnsiCodes.ANSI_CYAN, AnsiCodes.ANSI_YELLOW};
 
     public Character[] askForPlayers() {
         Character[] players = new Character[2];
 
         for (int i = 0; i < players.length; i++) {
-            System.out.print(COLORS[i] + ">>> ● Player " + (i + 1) + " <<<\n" + AnsiCodes.ANSI_RESET);
+            System.out.print(Character.getAnsiColorFromCharacter(i) + ">>> ● Player " + (i + 1) + " <<<\n" + AnsiCodes.ANSI_RESET);
             String name = read("Name: ");
-            players[i] = new Character(name, COLORS[i]);
+            players[i] = new Character(name);
             System.out.println();
         }
 
         return players;
+    }
+
+    public int askMoveFrom(Character player) {
+        String input = read(player.getAnsiColor() + player.getName() + AnsiCodes.ANSI_RESET);
+
     }
 
     private String read(String prompt) {

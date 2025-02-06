7f60977131bb34eaaad36ce539e472b515eaace6
Josep Fortuny <82282886+josep42ny@users.noreply.github.com>
2025-02-06 08:33:16 +0100

    remove view reference from gameController

--- src/josepfortuny/connectfour/GameController.java
+++ src/josepfortuny/connectfour/GameController.java
@@ -3,14 +3,12 @@ package josepfortuny.connectfour;
 public class GameController {
 
     InputHandler inputHandler;
-    View gameView;
     Board gameBoard;
     Character[] players;
 
     public void awake() {
 
         inputHandler = new InputHandler();
-        gameView = new View();
         gameBoard = new Board(6, 7);
         players = inputHandler.askForPlayers();
         update();
